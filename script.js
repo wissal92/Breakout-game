4818c59a66a25656ff499dee17b82ea2e9ac9a27
wissal <wissal.ouarsafi@gmail.com>
2020-04-09 21:19:57 -0700

    Create the move paddle functionality

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ const ctx = canvas.getContext('2d');
 
 let score = 0;
 
+const brickRowCount = 9;
+const brickColumnCount = 5;
+
 // Create ball props
 const ball = {
     x: canvas.width / 2,
@@ -51,17 +54,102 @@ function drawScore() {
     ctx.fillText(`Score: ${score}`, canvas.width - 100, 30);
 }
 
+// Create brick props
+const brickInfo = {
+    w: 70,
+    h: 20,
+    padding: 10,
+    offsetX: 45,
+    offsetY: 60,
+    visible: true
+};
+
+// Create bricks
+const bricks = [];
+for (let i = 0; i < brickRowCount; i++) {
+  bricks[i] = [];
+  for (let j = 0; j < brickColumnCount; j++) {
+    const x = i * (brickInfo.w + brickInfo.padding) + brickInfo.offsetX;
+    const y = j * (brickInfo.h + brickInfo.padding) + brickInfo.offsetY;
+    bricks[i][j] = { x, y, ...brickInfo };
+  }
+}
+
+// Draw bricks on canvas
+function drawBricks() {
+    bricks.forEach(column => {
+      column.forEach(brick => {
+        ctx.beginPath();
+        ctx.rect(brick.x, brick.y, brick.w, brick.h);
+        ctx.fillStyle = brick.visible ? '#0095dd' : 'transparent';
+        ctx.fill();
+        ctx.closePath();
+      });
+    });
+}
+
+// Move paddle on canvas
+function movePaddle() {
+    paddle.x += paddle.dx;
+  
+    // Wall detection
+    if (paddle.x + paddle.w > canvas.width) {
+      paddle.x = canvas.width - paddle.w;
+    }
+  
+    if (paddle.x < 0) {
+      paddle.x = 0;
+    }
+}
+
 // Draw everything
 function draw() {
-  
+   // clear canvas
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
     drawBall();
     drawPaddle();
     drawScore();
+    drawBricks();
 }
 
-draw();
+// Update canvas drawing and animation
+function update() {
+    movePaddle();
+    // moveBall();
   
+    // Draw everything
+    draw();
   
+    requestAnimationFrame(update);
+}
+  
+update();
+  
+// Keydown event
+function keyDown(e) {
+    if (e.key === 'Right' || e.key === 'ArrowRight') {
+      paddle.dx = paddle.speed;
+    } else if (e.key === 'Left' || e.key === 'ArrowLeft') {
+      paddle.dx = -paddle.speed;
+    }
+}
+
+// Keyup event
+function keyUp(e) {
+    if (
+      e.key === 'Right' ||
+      e.key === 'ArrowRight' ||
+      e.key === 'Left' ||
+      e.key === 'ArrowLeft'
+    ) {
+      paddle.dx = 0;
+    }
+}
+
+// Keyboard event handlers
+document.addEventListener('keydown', keyDown);
+document.addEventListener('keyup', keyUp);
 
 
 rulesBtn.addEventListener('click', () => rules.classList.add('show'));
